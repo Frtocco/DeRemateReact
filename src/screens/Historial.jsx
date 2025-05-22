77643c3f1ac8801fd4736946f42eab2db9c5c8be
Pedro <pedro.16011@gmail.com>
2025-05-22 17:51:39 -0300

    eliminado de lista estatica en hisorial

--- src/screens/Historial.jsx
+++ src/screens/Historial.jsx
@@ -7,19 +7,6 @@ import {useAxios} from '../hooks/UseAxios';
 import { ActivityIndicator } from 'react-native';
 
 
-
-
-const orders = [
-  { orderId: '1', status: 'Delivered', address: '742 Evergreen Terrace' },
-  { orderId: '2', status: 'In Progress', address: 'Springfield Mall' },
-  { orderId: '3', status: 'Cancelled', address: 'Krusty Burger' },
-  { orderId: '1', status: 'Delivered', address: '742 Evergreen Terrace' },
-  { orderId: '2', status: 'In Progress', address: 'Springfield Mall' },
-  { orderId: '3', status: 'Cancelled', address: 'Krusty Burger' },
-  { orderId: '1', status: 'Delivered', address: '742 Evergreen Terrace' },
-  { orderId: '2', status: 'In Progress', address: 'Springfield Mall' },
-];
-
 const Historial = ({ navigation }) => {
 
   const [lista, setLista] = useState([]);
@@ -30,7 +17,7 @@ const Historial = ({ navigation }) => {
       const fetchOrders = async () => {
       try {
         const response = await axios.get('http://localhost:1234/orders/history?riderId=c7501d98-ef08-47ec-8e59-c4e9ae110eda'); // Cambia la URL por la de tu API real
-        setLista(response.data); // Asegúrate que response.data sea un array de órdenes
+        setLista(response.data); 
       } catch (error) {
         console.error('Error fetching orders:', error);
       } finally {
