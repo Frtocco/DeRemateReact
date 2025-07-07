ebc6b593dd50c0c1819d383b40f57bbe917ecdc4
AgustinCastino <aguscastino01@gmail.com>
2025-07-07 00:02:47 -0300

    Se agrega llamado a los componentes nuevos

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ConfirmDeliveryButton from './ConfirmDeliveryButton';
+import OpenInMapsButton from './OpenInMapsButton'
+
 
 const OrderCard = ({ order }) => (
   <View style={styles.card}>
     <Text style={styles.title}>Orden #{order.orderId}</Text>
     <Text style={styles.status}>Estado: {order.status}</Text>
     <Text style={styles.address}>Direccion: {order.addressDesc}</Text>
+    {order.status === 'In Progress' && (
+      <View style={styles.buttonGroup}>
+        <ConfirmDeliveryButton order={order} />
+        <OpenInMapsButton order={order} />
+      </View>
+    )}
   </View>
 );
 
@@ -33,6 +42,32 @@ const styles = StyleSheet.create({
   address: {
     fontSize: 16,
     color: '#555',
+  },
+    buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
+    gap: 10,
+  },
+  confirmButton: {
+    backgroundColor: '#28a745',
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+    flex: 1,
+    alignItems: 'center',
+  },
+  mapsButton: {
+    backgroundColor: '#007bff',
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+    flex: 1,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
